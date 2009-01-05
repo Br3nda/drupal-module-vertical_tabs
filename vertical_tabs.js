4766f5875f462cfb4f88463d0a25f6791ac1f315
quicksketch <quicksketch>
2009-01-05 21:15:32 +0000

    #351456 and #338283. Code cleanup regarding theme-specific styling and preping for color.module enhancement.

--- vertical_tabs.js
+++ vertical_tabs.js
@@ -2,7 +2,8 @@ Drupal.verticalTabs = Drupal.verticalTabs || {};
 
 Drupal.behaviors.verticalTabs = function() {
   if (!$('.vertical-tabs-list').size()) {
-    var ul = $('<div class="vertical-tabs"><ul class="vertical-tabs-list"></ul></div>').find('ul');
+    var ul = $('<ul class="vertical-tabs-list"></ul>');
+    var fieldsets = $('<div class="vertical-tabs-fieldsets"></div>');
     $.each(Drupal.settings.verticalTabs, function(k, v) {
       var description = '', cssClass = 'vertical-tabs-list-' + k;
       if (v.callback && Drupal.verticalTabs[v.callback]) {
@@ -11,29 +12,38 @@ Drupal.behaviors.verticalTabs = function() {
       else {
         cssClass += ' vertical-tabs-nodescription';
       }
-      ul.append($('<li><a href="#' + k + '" class="' + cssClass + '">'+ v.name + description +'</a></li>')
-          .find('a')
-          .bind('click', function() { $(this).parent().addClass('selected').siblings().removeClass('selected'); $('.vertical-tabs-' + k).show().siblings('.vertical-tabs-div').hide(); return false; })
-          .end())
-        .end()
-        .append($('.vertical-tabs-' + k + ' > .fieldset-wrapper')
-        .addClass('vertical-tabs-' + k)
-        .addClass('vertical-tabs-div'));
+
+      // Add a list item to the vertical tabs list.
+      $('<li><a href="#' + k + '" class="' + cssClass + '">'+ v.name + description +'</a></li>').appendTo(ul)
+        .find('a')
+        .bind('click', function() {
+          $(this).parent().addClass('selected').siblings().removeClass('selected');
+          $('.vertical-tabs-' + k).show().siblings('.vertical-tabs-div').hide();
+          return false;
+        });
+
+      // Add the fieldset contents to the toggled fieldsets.
+      $('.vertical-tabs-' + k + ' > .fieldset-wrapper').appendTo(fieldsets)
+      .addClass('vertical-tabs-' + k)
+      .addClass('vertical-tabs-div');
+
       if (v.callback && Drupal.verticalTabs[v.callback]) {
-        ul.end()
-          .bind('change', function() { $('.vertical-tabs-list-' + k + ' > .description').html(Drupal.verticalTabs[v.callback].apply(this, v.args)); })
-          .end()
-          .find('ul');
+        ul.bind('change', function() { $('.vertical-tabs-list-' + k + ' > .description').html(Drupal.verticalTabs[v.callback].apply(this, v.args)); })
       }
       $('.vertical-tabs-' + k).remove();
     });
-    ul.end();
-    $('div.vertical-tabs').replaceWith(ul.parent());
+
+    $('div.vertical-tabs').html(ul).append(fieldsets);
+
+    // Adjust the height of the active field area to be no less than the
+    // total height of all the tabs.
     var max = Math.max($('.vertical-tabs ul').height() - 12, $('.vertical-tabs-div:first').height());
     $('.vertical-tabs-div').each(function() {
       max = Math.max(max, $(this).height());
     });
     $('.vertical-tabs-div').height(max).hide();
+
+    // Activate the first tab.
     $('.vertical-tabs-div:first').show();
     $('.vertical-tabs ul li:first').addClass('selected');
   }
