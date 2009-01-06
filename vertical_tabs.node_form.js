08cf30eb2fc17a42eca1b1590f4e95aadf4dc6fa
quicksketch <quicksketch>
2009-01-06 02:47:47 +0000

    Adding taxonomy support.

--- vertical_tabs.node_form.js
+++ vertical_tabs.node_form.js
@@ -100,3 +100,40 @@ Drupal.verticalTabs.flag = function() {
     return Drupal.t('No flags');
   }
 }
+
+
+Drupal.verticalTabs.taxonomy = function() {
+  var terms = {};
+  var termCount = 0;
+  $('div.vertical-tabs-taxonomy').find('select, input.form-text').each(function() {
+    if (this.value) {
+      var vocabulary = $(this).siblings('label').html();
+      terms[vocabulary] = terms[vocabulary] || [];
+      if ($(this).is('input.form-text')) {
+        terms[vocabulary].push(this.value);
+        termCount++;
+      }
+      else if ($(this).is('select')) {
+        $(this).find('option[selected]').each(function() {
+          terms[vocabulary].push($(this).text());
+          termCount++;
+        });
+      }
+    }
+  });
+
+  if (termCount) {
+    var output = '';
+    $.each(terms, function(vocab, vocab_terms) {
+      if (output) {
+        output += '<br />';
+      }
+      output += vocab;
+      output += vocab_terms.join(', ');
+    });
+    return output;
+  }
+  else {
+    return Drupal.t('No terms');
+  }
+}
