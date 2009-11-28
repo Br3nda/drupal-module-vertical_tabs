cf610e15f0cf7b074e6892450e420eeba9f03660
davereid <davereid>
2009-11-28 22:10:47 +0000

    by Dave Reid: Show 'Anonymous' username if authored by field is empty.

--- vertical_tabs.node_form.js
+++ vertical_tabs.node_form.js
@@ -1,4 +1,4 @@
-// $Id: vertical_tabs.node_form.js,v 1.1.2.9 2009/11/18 20:06:45 davereid Exp $
+// $Id: vertical_tabs.node_form.js,v 1.1.2.10 2009/11/28 22:10:47 davereid Exp $
 
 Drupal.verticalTabs = Drupal.verticalTabs || {};
 
@@ -28,12 +28,12 @@ Drupal.verticalTabs.revision_information = function() {
 }
 
 Drupal.verticalTabs.author = function() {
-  var name = $('#edit-name').val(), date = $('#edit-date').val();
+  var author = $('#edit-name').val() || Drupal.t('Anonymous'), date = $('#edit-date').val();
   if (date) {
-    return Drupal.t('By @name on @date', { '@name': name, '@date': date });
+    return Drupal.t('By @name on @date', { '@name': author, '@date': date });
   }
   else {
-    return Drupal.t('By @name', { '@name': name });
+    return Drupal.t('By @name', { '@name': author });
   }
 }
 
